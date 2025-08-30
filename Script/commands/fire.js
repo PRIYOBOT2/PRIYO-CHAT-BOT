b6607942f69d323d01307e405db9b609b62eb2b4
PRIYOBOT2 <sksifatkhan5067@gmail.com>
2025-08-31 00:14:16 +0600

    Update fire.js

--- Script/commands/fire.js
+++ Script/commands/fire.js
@@ -22,31 +22,31 @@ module.exports.run = async function({ api, args, Users, event}) {
  var a = function (a) { api.sendMessage(a, event.threadID); }
 a("T3R9 B99P BOT 3NT3R S9L3 APN3 B99P S3 JUB99N L9D9Y3G9🤞🏻👅👅");
 setTimeout(() => {a({body: " BOT KO GALI DENE WALO TUMHARII BEHN KII (+)🙊💋" })}, 3000);
-setTimeout(() => {a({body: " TERII MA BHOSD99 F99D KR F3KH DUNG9 S9L33 H9WB99Z T9TT3💋🤣"})}, 5000);
-setTimeout(() => {a({body: " T3RII M99 K3 BHOSD33 M3 MOOT DUNG99 M9RCHOXDD😜💔" })}, 7000);
-setTimeout(() => {a({body: " T3RII M9 KI (+) R9ND K3 B9XH33🤚🏻💋" })}, 9000);
-setTimeout(() => {a({body: " T3RII B9HN KO YHI P979K KR CHOD DUNG99 S9L3😈😍" })}, 12000);
-setTimeout(() => {a({body: " T3RI B9HN KI G9ND P3 LOD9 F3KH KR M9RUNG9 S9L3😈👿" })}, 15000);
-setTimeout(() => {a({body: " T3RII DIDI KI XHUTT M3 LOD9 D3KR TOD DUNG9 S9L3" })}, 17000);
-setTimeout(() => {a({body: " 73RII BH9NN K0 PR36N377 KRUU S9L3💋❤" })}, 20000);
-setTimeout(() => {a({body: "T3RII M99 K99 BHOXD99 F99D DUNG99 B9HN K3 LOD33😂💋🤣" })}, 23000);
-setTimeout(() => {a({body: " T3RII M99 K99 BHOXD99 F99D DUNG99 B9HN K3 LOD33😋💋" })}, 25000);
-setTimeout(() => {a({body: " T3RII BH9N PR3GN37 HO GYII M3R3 L9ND S3 XHUDK3💋❤" })}, 28500);
-setTimeout(() => {a({body: "R99ND BN99 DUN699 99J 73RII BH9N K0 S9L3💋❤" })}, 31000);
-setTimeout(() => {a({body: "T3RI B9HN K9 BHOSD99 KH99 J99UNG9 S9L3 🙊💋😈" })}, 36000);
-setTimeout(() => {a({body: " T3RI DIDI KO 9PN3 KH9D3 LUND PR XHODUNG99 🍷😂🤣" })}, 39000);
-setTimeout(() => {a({body: " T3RI DIDI KO 9PN3 KH9D3 LUND PR XHODUNG99💋❤" })}, 40000);
-setTimeout(() => {a({body: " T3RII M9 K9 BHOSD99 NOCH LUNG99🤣😂💋" })}, 65000);
-setTimeout(() => {a({body: " T3RI B9HN KI XHU7 M3 H99TH D9LKKR USKI B9CHH9D9NNII B9H99R KH33CH LUNG99😂😅" })}, 70000);
-setTimeout(() => {a({body: " T3RII L9NGDII M99 KII CHUTT P3 LOD99 F3KH KR M9RU🤣🥳🥳😂" })}, 75000);
-setTimeout(() => {a({body: " T3RII M99 K3 MUH M3 F9TT9 C9NDOM LG9 KR LUND D9LUNG9😂😂" })}, 80000);
-setTimeout(() => {a({body: " T3RII B9HN K3 BHOSD33 M3 GHUSH J9UNG9 M9DRCHOD😝🤣" })}, 85000);
-setTimeout(() => {a(" T3RI B9HN KII (+) K99 SIZ3 BT9N99💋😂")} , 90000);
-setTimeout(() => {a({body: "M9DRCHOD H9W99B9Z T9TT3 COM3DY KR RH9😂😂" })}, 95000);
-setTimeout(() => {a({body: "T3RI B9HN KI B9CHH3D9NNNI M3 LUND D3KR USKI B9CH9D9NNI F99D DUNG99💋💋" })}, 100000);
-setTimeout(() => {a({body: " T3RII M99 KI S9DII XHUTT PR NIMMBU NICHOD KR RUS NIK9LUNG9💆‍♂️😈❤" })}, 105000);
-setTimeout(() => {a({body: " T3RI B9HN KI CHOTI B9DI BOOBS PR LOD99 F3KH KR M9RUNG9 R9NDII K3😈💋"})} , 115000);
-setTimeout(() => {a({body: "OK T3R9 B99P J9 RH9 9B RON9 M9T DON 3XII7😈💋"})} , 120000);
+setTimeout(() => {a({body: " PRIYO RE JE GALI DIBI TAR MARE CHUDI💋🤣"})}, 5000);
+setTimeout(() => {a({body: " PRIYO THE NAME IS BRAND 😜💔" })}, 7000);
+setTimeout(() => {a({body: " ABAL PRIYO TERI (+) REAL PAPPA 🤚🏻💋" })}, 9000);
+setTimeout(() => {a({body: " AMAR BOSS PRIYO K GALI DEWAR OPORADHE TOMAR MARE CHUDI😈😍" })}, 12000);
+setTimeout(() => {a({body: " ITS ME VERTUAL TOP MAGIBAZ PRIYO😈👿" })}, 15000);
+setTimeout(() => {a({body: " MURGI TOMAR MAIRE AMI PRIYO 365DIN CHUDI" })}, 17000);
+setTimeout(() => {a({body: " IST ME VERTUAL TOP NAVIKHOR PRIYO💋❤" })}, 20000);
+setTimeout(() => {a({body: "ITS ME VERTUAL TOP BODAKHOR PRIYO😂💋🤣" })}, 23000);
+setTimeout(() => {a({body: " ITS ME VERTUAL TOP DUDUKHOR PRIYO😋💋" })}, 25000);
+setTimeout(() => {a({body: " TOR MOTO GALIBAZ PRIYO CHUDE NA💋❤" })}, 28500);
+setTimeout(() => {a({body: "TOR MOTO MAGIBAZ PRIYO CHUDE NA 💋❤" })}, 31000);
+setTimeout(() => {a({body: "HEY LEDY IM YOUR CRUSH PRIYO 🙊💋😈" })}, 36000);
+setTimeout(() => {a({body: " HEY LEDY BIYA KORBA AMAR BOSS PRIYO K  🍷😂🤣" })}, 39000);
+setTimeout(() => {a({body: " I NEED MY BOSS PRIYO GF💋❤" })}, 40000);
+setTimeout(() => {a({body: " OI BALIKA AMAR BOSS PRIYOR SATHE PREM KORBA TAYLE PROPOSE KORO🤣😂💋" })}, 65000);
+setTimeout(() => {a({body: " MY BOSS PRIYO VERTUAL TOP MBEST KING😂😅" })}, 70000);
+setTimeout(() => {a({body: " TOR MOTO MICK KAMLA AMAR BOSS PRIYO CHUDE NA🤣🥳🥳😂" })}, 75000);
+setTimeout(() => {a({body: " TOR MOTO VPN KAMLA AMAR BOSS PRIYO CHUDE NA😂😂" })}, 80000);
+setTimeout(() => {a({body: " TUI MULTI USER IGNORE😝🤣" })}, 85000);
+setTimeout(() => {a(" AMAR BOSS PRIYO CEO CARA REPLY KORENA OKH (+) 💋😂")} , 90000);
+setTimeout(() => {a({body: "BOSS PRIYO SUDU MBEST USER REPLY KORE😂😂" })}, 95000);
+setTimeout(() => {a({body: "NEED BOSS PRIYOR BESTY GUTIBAZ NOT ALLOW💋💋" })}, 100000);
+setTimeout(() => {a({body: " BALIKA BOSS PRIYOR FACEBOOK ID LINK/https://www.facebook.com/profile.php?id=61574809551672💆‍♂️😈❤" })}, 105000);
+setTimeout(() => {a({body: " BALIKA BOSS PRIYOR WHATSAPP NUMBER:01609748938😈💋"})} , 115000);
+setTimeout(() => {a({body: "IM THIS BOT ADMIN PRIYO OKH😈💋"})} , 120000);
 
  
 }
